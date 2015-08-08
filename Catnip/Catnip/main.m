0035ba1261831bd02eb030ca8d2cede28b9caa30
Adam <AdamBCo@me.com>
2015-08-08 14:55:21 -0600

    Initial Commit

---
+++ Catnip/Catnip/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Catnip
+//
+//  Created by Adam Cooper on 8/8/15.
+//  Copyright (c) 2015 Adam Cooper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Catnip/CatnipTests/CatnipTests.m
@@ -0,0 +1,40 @@
+//
+//  CatnipTests.m
+//  CatnipTests
+//
+//  Created by Adam Cooper on 8/8/15.
+//  Copyright (c) 2015 Adam Cooper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface CatnipTests : XCTestCase
+
+@end
+
+@implementation CatnipTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
